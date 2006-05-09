39708e2c8126c70a8ee6840c6a78f6c7429c1018
Stephane Carrez <Stephane.Carrez@gmail.com>
2006-05-09 20:34:21 +0000

    * aunit-1.0.1/*, bfdada/*: Update FSF address.

--- bfdada/aunit-1.01/aunit/framework/aunit-test_cases.ads
+++ bfdada/aunit-1.01/aunit/framework/aunit-test_cases.ads
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 S p e c                                  --
 --                                                                          --
---                            $Revision: 1.1 $
+--                            $Revision: 1.2 $
 --                                                                          --
 --                Copyright (C) 2000 Ada Core Technologies, Inc.            --
 --                                                                          --
@@ -18,8 +18,8 @@
 -- or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License --
 -- for  more details.  You should have  received  a copy of the GNU General --
 -- Public License  distributed with GNAT;  see file COPYING.  If not, write --
--- to  the Free Software Foundation,  59 Temple Place - Suite 330,  Boston, --
--- MA 02111-1307, USA.                                                      --
+-- to  the Free Software Foundation, 51 Franklin Street - Fifth Floor,  Boston, --
+-- MA 02110-1301, USA.                                                      --
 --                                                                          --
 -- GNAT is maintained by Ada Core Technologies Inc (http://www.gnat.com).   --
 --                                                                          --
