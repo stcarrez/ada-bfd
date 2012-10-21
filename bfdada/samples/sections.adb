d491062b952cf02915e5cd4cd69d1b2ca2ed6c87
Stephane Carrez <Stephane.Carrez@gmail.com>
2012-10-21 18:32:13 +0000

    Simplify the cleanup the sample

--- bfdada/samples/sections.adb
+++ bfdada/samples/sections.adb
@@ -1,7 +1,7 @@
 -----------------------------------------------------------------------
 --  sections -- Simple Example to list ELF sections
---  Copyright (C) 2002, 2003, 2004 Free Software Foundation, Inc.
---  Written by Stephane Carrez (stcarrez@nerim.fr)
+--  Copyright (C) 2002, 2003, 2004, 2012 Free Software Foundation, Inc.
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  This file is part of BfdAda.
 --
@@ -20,182 +20,108 @@
 --  the Free Software Foundation,51 Franklin Street - Fifth Floor,
 --  Boston, MA 02110-1301, USA.
 -----------------------------------------------------------------------
-with Ada.Command_Line;    use Ada.Command_Line;
-with Ada.IO_Exceptions;
-with GNAT.Command_Line;
+with Ada.Command_Line;
+with Ada.Text_IO;
 
-with Interfaces; use Interfaces;
-with Bfd; use Bfd;
-with Bfd.Sections; use Bfd.Sections;
-with Bfd.Symtab; use Bfd.Symtab;
-with Ada.Text_IO; use Ada.Text_IO;
+with Bfd;
+with Bfd.Sections;
 
-with Utils; use Utils;
+with Utils;
 procedure Sections is
-
-
-   Opt_H : Boolean := False;
-   Opt_V : Boolean := False;
 
    RC : Ada.Command_Line.Exit_Status := 0;
 
-   procedure Usage;
-   procedure List_Section (File : Bfd.File_Type);
-   procedure Parse_Arguments;
-
-   --------------------------------------------------
-   --  Usage
-   --------------------------------------------------
-   procedure Usage is
-      use Ada.Text_IO;
-      use Ada.Command_Line;
-   begin
-      New_Line;
-      Put ("Usage: ");
-      Put (Command_Name);
-      Put_Line (" [-h] file ...");
-      Put_Line ("Lists the sections contained in a binary file");
-      Put_Line ("where:");
-      Put_Line ("   -h           Verbose");
-      New_Line;
-      RC := 2;
-   end Usage;
+   procedure List_Section (File : in Bfd.File_Type);
 
    --------------------------------------------------
    --  List the sections of the BFD file
    --------------------------------------------------
-   procedure List_Section (File : Bfd.File_Type) is
-      Iter : Section_Iterator := Get_Sections (File);
+   procedure List_Section (File : in Bfd.File_Type) is
+      use type Bfd.Sections.Section_Flags;
+
+      Iter : Bfd.Sections.Section_Iterator := Bfd.Sections.Get_Sections (File);
    begin
-      Print ("Name", 30);
-      Print ("Size", -10);
-      Print ("VMA", -17);
-      Print ("LMA", -17);
-      Print ("Flags", -10);
-      New_Line;
-      Print ("====", 30);
-      Print ("====", -10);
-      Print ("===", -17);
-      Print ("===", -17);
-      Print ("=====", -10);
-      New_Line;
-      while not Is_Done (Iter) loop
+      Utils.Print ("Name", 30);
+      Utils.Print ("Size", -10);
+      Utils.Print ("VMA", -17);
+      Utils.Print ("LMA", -17);
+      Utils.Print ("Flags", -10);
+      Ada.Text_IO.New_Line;
+      Utils.Print ("====", 30);
+      Utils.Print ("====", -10);
+      Utils.Print ("===", -17);
+      Utils.Print ("===", -17);
+      Utils.Print ("=====", -10);
+      Ada.Text_IO.New_Line;
+      while Bfd.Sections.Has_Element (Iter) loop
          declare
-            S : Section := Current (Iter);
-            P : String (1 .. 6) := (others => ' ');
+            S   : constant Bfd.Sections.Section := Bfd.Sections.Element (Iter);
+            P   : String (1 .. 6) := (others => ' ');
             Pos : Positive := 1;
          begin
-            Print (Get_Name (S), 30);
-            Print (Size_Type'Image (S.Size), -10);
-            Print (HexImage (S.Vma), -17);
-            Print (HexImage (S.Lma), -17);
+            Utils.Print (Bfd.Sections.Get_Name (S), 30);
+            Utils.Print (Bfd.Size_Type'Image (S.Size), -10);
+            Utils.Print (Utils.HexImage (S.Vma), -17);
+            Utils.Print (Utils.HexImage (S.Lma), -17);
 
-            if (S.Flags and SEC_ALLOC) /= 0 then
+            if (S.Flags and Bfd.Sections.SEC_ALLOC) /= 0 then
                P (Pos) := 'A';
                Pos := Pos + 1;
             end if;
 
-            if (S.Flags and SEC_LOAD) /= 0 then
+            if (S.Flags and Bfd.Sections.SEC_LOAD) /= 0 then
                P (Pos) := 'L';
                Pos := Pos + 1;
             end if;
 
-            if (S.Flags and SEC_READONLY) /= 0 then
+            if (S.Flags and Bfd.Sections.SEC_READONLY) /= 0 then
                P (Pos) := 'R';
                Pos := Pos + 1;
             end if;
 
-            if (S.Flags and SEC_DATA) /= 0 then
+            if (S.Flags and Bfd.Sections.SEC_DATA) /= 0 then
                P (Pos) := 'W';
                Pos := Pos + 1;
             end if;
 
-            if (S.Flags and SEC_CODE) /= 0 then
+            if (S.Flags and Bfd.Sections.SEC_CODE) /= 0 then
                P (Pos) := 'X';
                Pos := Pos + 1;
             end if;
 
-            Print (P, -10);
-            New_Line;
+            Utils.Print (P, -10);
+            Ada.Text_IO.New_Line;
          end;
-         Next (Iter);
+         Bfd.Sections.Next (Iter);
       end loop;
    end List_Section;
 
-   --------------------------------------------------
-   --  Parse_Arguments
-   --------------------------------------------------
-   procedure Parse_Arguments is
-      use Ada.Text_IO;
-      use Ada.Command_Line;
-      use GNAT.Command_Line;
-
-      Optch :  Character;
-   begin
-      ------------------------------
-      --  Process command line options.
-      ------------------------------
-      Initialize_Option_Scan (Stop_At_First_Non_Switch => True);
-
-      begin
-         loop
-            Optch := Getopt ("h ");
-
-            case Optch is
-               when Standard.Ascii.NUL =>
-                  exit;
-               when 'h' =>
-                  Opt_H := True;
-               when others =>
-                  raise Program_Error;
-            end case;
-         end loop;
-      exception
-         when Invalid_Switch =>
-            RC := 1;
-            Put_Line (Standard_Error, "Invalid option: -" & Full_Switch);
-         when Invalid_Parameter =>
-            RC := 1;
-            Put_Line (Standard_Error, "Missing argument: -" & Full_Switch);
-      end;
-
-      ------------------------------
-      --  If -v, then show program release
-      ------------------------------
-      if Opt_H then
-         Usage;
-      end if;
-
-      --  Open each file passed as argument and try dumping its
-      --  sections and symbol table.
-      loop
-         declare
-            Arg  : String := Get_Argument;
-            File : Bfd.File_Type;
-         begin
-            exit when Arg = "";
-
-            Open (File, Arg, "");
-            if Check_Format (File, OBJECT) then
-               List_Section (File);
-            end if;
-            Close (File);
-
-         exception
-            when OPEN_ERROR =>
-               Put_Line (Standard_Error, "Cannot open file '" & Arg
-                         & "': " & Get_Error_Message (Get_Error));
-         end;
-      end loop;
-   end Parse_Arguments;
+   Count  : constant Natural := Ada.Command_Line.Argument_Count;
 
 begin
-   Set_Error_Program_Name (To => "sections");
+   Bfd.Set_Error_Program_Name (To => "sections");
 
-   Parse_Arguments;
-   if RC /= 0 then
-      Set_Exit_Status (RC);
-      return;
-   end if;
+   --  Open each file passed as argument and try dumping its
+   --  sections and symbol table.
+   for I in 1 .. Count loop
+      declare
+         Path : constant String := Ada.Command_Line.Argument (I);
+         File : Bfd.File_Type;
+      begin
+         Bfd.Open (File, Path, "");
+         if Bfd.Check_Format (File, Bfd.OBJECT) then
+            List_Section (File);
+         end if;
+         Bfd.Close (File);
 
+      exception
+         when Bfd.OPEN_ERROR =>
+            Ada.Text_IO.Put_Line (Ada.Text_IO.Standard_Error,
+                                  "Cannot open file '" & Path
+                                  & "': " & Bfd.Get_Error_Message (Bfd.Get_Error));
+            RC := 1;
+      end;
+   end loop;
+
+   Ada.Command_Line.Set_Exit_Status (RC);
 end Sections;
