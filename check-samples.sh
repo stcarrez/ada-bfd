0e3fa0480d9bc0c97ccdc2522f616f99d7bb774d
Stephane Carrez <Stephane.Carrez@gmail.com>
2021-04-10 18:26:03 +0200

    Simple script to check the samples

---
+++ check-samples.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+T=`bin/sections bin/bfdgen | grep .text | wc -l`
+if test $T -ne 1; then
+    echo "Test samples/sections failed"
+    exit 1
+fi
+
+T=`bin/bfdinfo bin/bfdgen | grep .rodata | wc -l`
+if test $T -ne 2; then
+    echo "Test samples/bfdinfo failed"
+    exit 1
+fi
+
+T=`bin/symbol bin/bfdgen main | grep global | wc -l`
+if test $T -ne 1; then
+    echo "Test samples/symbol failed"
+    exit 1
+fi
+
+T=`bin/disassemble bin/bfdgen | grep support/bfdgen.c | wc -l`
+if test $T -ne 1; then
+    echo "Test samples/disassemble failed"
+    exit 1
+fi
+
+T=`bin/disassemble bin/bfdgen | grep bfdgen.c | wc -l`
+if test $T -lt 10; then
+    echo "Test samples/disassemble failed"
+    exit 1
+fi
+
