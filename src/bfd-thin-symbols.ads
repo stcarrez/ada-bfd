27488351348a1d39b0277d4023f768e035e321da
Stephane Carrez <Stephane.Carrez@gmail.com>
2021-04-11 16:52:52 +0200

    Update to use Ada 2012 aspects

--- src/bfd-thin-symbols.ads
+++ src/bfd-thin-symbols.ads
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  BFD -- Thin Ada layer for Bfd (common Bfd functions)
---  Copyright (C) 2002, 2003, 2004, 2012, 2015 Free Software Foundation, Inc.
+--  Copyright (C) 2002, 2003, 2004, 2012, 2015, 2021 Free Software Foundation, Inc.
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  This file is part of BfdAda.
@@ -27,25 +27,25 @@ with Bfd.Sections;
 with Bfd.Symbols;
 package Bfd.Thin.Symbols is
 
-   function Get_Symbol_Name (Sym : in Bfd.Symbols.Symbol) return Interfaces.C.Strings.chars_ptr;
-   pragma Import (C, Get_Symbol_Name, "ada_bfd_asymbol_name");
+   function Get_Symbol_Name (Sym : in Bfd.Symbols.Symbol) return Interfaces.C.Strings.chars_ptr
+     with Import => True, Convention => C, Link_Name => "ada_bfd_asymbol_name";
 
-   function Get_Symbol_Section (Sym : in Bfd.Symbols.Symbol) return Bfd.Sections.Section_Iterator;
-   pragma Import (C, Get_Symbol_Section, "ada_bfd_asymbol_section");
+   function Get_Symbol_Section (Sym : in Bfd.Symbols.Symbol) return Bfd.Sections.Section_Iterator
+     with Import => True, Convention => C, Link_Name => "ada_bfd_asymbol_section";
 
-   function Is_Local (P : Ptr; Sym : in Bfd.Symbols.Symbol) return Interfaces.C.int;
-   pragma Import (C, Is_Local, "bfd_is_local_label");
+   function Is_Local (P : Ptr; Sym : in Bfd.Symbols.Symbol) return Interfaces.C.int
+     with Import => True, Convention => C, Link_Name => "bfd_is_local_label";
 
-   function Get_Symbol_Value (Sym : in Bfd.Symbols.Symbol) return Symbol_Value;
-   pragma Import (C, Get_Symbol_Value, "ada_bfd_asymbol_value");
+   function Get_Symbol_Value (Sym : in Bfd.Symbols.Symbol) return Symbol_Value
+     with Import => True, Convention => C, Link_Name => "ada_bfd_asymbol_value";
 
-   function Get_Symbol_Size (Sym : in Bfd.Symbols.Symbol) return Symbol_Value;
-   pragma Import (C, Get_Symbol_Size, "ada_bfd_asymbol_size");
+   function Get_Symbol_Size (Sym : in Bfd.Symbols.Symbol) return Symbol_Value
+     with Import => True, Convention => C, Link_Name => "ada_bfd_asymbol_size";
 
    procedure Read_Symbols (File : Ptr;
                            Cnt : System.Address;
-                           S : System.Address);
-   pragma Import (C, Read_Symbols, "bfd_read_symbols");
+                           S : System.Address)
+     with Import => True, Convention => C, Link_Name => "bfd_read_symbols";
 
    procedure Find_Nearest_Line (File : in Ptr;
                                 Sec : in Bfd.Sections.Section_Iterator;
@@ -53,18 +53,15 @@ package Bfd.Thin.Symbols is
                                 Addr : in Vma_Type;
                                 Name : in System.Address;
                                 Func : in System.Address;
-                                Line : in Ptr);
-   pragma Import (C, Find_Nearest_Line, "bfd_find_nearest_line_");
+                                Line : in Ptr)
+     with Import => True, Convention => C, Link_Name => "bfd_find_nearest_line_";
 
-   --  function Is_Local (P : Ptr; Name : in String) return Boolean;
-   --  pragma Import (C, Is_Local, "bfd_is_local_label_name");
-
-   function Get_Symtab_Upper_Bound (File : in Ptr) return Integer;
-   pragma Import (C, Get_Symtab_Upper_Bound, "ada_bfd_get_symtab_upper_bound");
+   function Get_Symtab_Upper_Bound (File : in Ptr) return Integer
+     with Import => True, Convention => C, Link_Name => "ada_bfd_get_symtab_upper_bound";
 
    function Demangle (File    : in Ptr;
                       Name    : in Interfaces.C.Strings.chars_ptr;
-                      Options : in Bfd.Demangle_Flags) return Interfaces.C.Strings.chars_ptr;
-   pragma Import (C, Demangle, "bfd_demangle");
+                      Options : in Bfd.Demangle_Flags) return Interfaces.C.Strings.chars_ptr
+     with Import => True, Convention => C, Link_Name => "bfd_demangle";
 
 end Bfd.Thin.Symbols;
