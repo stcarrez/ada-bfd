910a2475246f5ce02196a79b285366205698a808
Stephane Carrez <Stephane.Carrez@gmail.com>
2023-08-16 21:40:01 +0200

    Update coverage script

--- coverage.sh
+++ coverage.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 NAME=bfdada.cov
-lcov --quiet --base-directory . --directory . -c -o $NAME
-lcov --quiet --remove $NAME "/usr*" -o $NAME
-lcov --quiet --remove $NAME "/build*" -o $NAME
-lcov --quiet --remove $NAME "/opt*" -o $NAME
-lcov --quiet --remove $NAME "*/adainclude*" -o $NAME
-lcov --quiet --remove $NAME "*/regtests*" -o $NAME
-lcov --quiet --remove $NAME "*/b__*" -o $NAME
-lcov --quiet --remove $NAME "*/testutil*" -o $NAME
+lcov --quiet --base-directory . --directory . \
+   --no-external \
+   --exclude '*/b__*.adb' \
+   --exclude '*/samples/*' \
+   --exclude '*/regtests*' \
+   --exclude '*/testutil/*' -c -o $NAME
 rm -rf cover
 genhtml --quiet --ignore-errors source -o ./cover -t "test coverage" --num-spaces 4 $NAME
- 
