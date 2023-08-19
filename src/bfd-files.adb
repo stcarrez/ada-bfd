fe3018ac728f1def54ee0a6f4871dc84db944a60
Stephane Carrez <Stephane.Carrez@gmail.com>
2023-08-19 18:14:33 +0200

    Add -liberty for the link

--- src/bfd-files.adb
+++ src/bfd-files.adb
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  BFD -- Binary File Descriptor Library (Ada Interface)
---  Copyright (C) 2001, 2002, 2003, 2004, 2012, 2014, 2019 Free Software Foundation, Inc.
+--  Copyright (C) 2001, 2002, 2003, 2004, 2012, 2014, 2019, 2023 Free Software Foundation, Inc.
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  This file is part of BfdAda.
@@ -31,6 +31,7 @@ package body Bfd.Files is
 
    pragma Linker_Options ("-lz");
    pragma Linker_Options ("-lbfd" & Bfd.Constants.LINKER_OPTIONS);
+   pragma Linker_Options ("-liberty");
 
    use type System.Address;
 
