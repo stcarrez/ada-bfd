a88b5163fa430dfe4170dd73d6dbb98e910637b9
Stephane Carrez <Stephane.Carrez@gmail.com>
2025-11-02 10:00:28 +0100

    Fix style compilation warning

--- src/bfd-files.adb
+++ src/bfd-files.adb
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  BFD -- Binary File Descriptor Library (Ada Interface)
---  Copyright (C) 2001, 2002, 2003, 2004, 2012, 2014, 2019, 2023, 2025 Free Software Foundation, Inc.
+--  Copyright (C) 2001 - 2025 Free Software Foundation, Inc.
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  This file is part of BfdAda.
